893ed3dbe885b914a2f43d4db42aaba6bde182c9
johnjovero98 <john.jovero@outlook.com>
2025-03-31 23:18:14 -0600

    Shop page: Add Filters on page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -45,64 +45,64 @@ get_header('shop');
 		?>
 	</div>
 
+	<div class="flex flex-col lg:flex-row gap-3 items-start">
+		<!-- Shop page filter  -->
+		<div class="shop-page-filter">
+			<?php dynamic_sidebar('product-filter') ?>
+		</div>
 
-	<!-- Shop page filter  -->
-	<div class="shop-page-filter">
-		<?php dynamic_sidebar('product-filter') ?>
-	</div>
+		<!-- Shop page content (product cards) -->
+		<div class="shop-page-content">
+			<?php
+			if (woocommerce_product_loop()) {
 
+				/**
+				 * Hook: woocommerce_before_shop_loop.
+				 *
+				 * @hooked woocommerce_output_all_notices - 10
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action('woocommerce_before_shop_loop');
 
-	<div class="shop-page-content">
-		<?php
-		if (woocommerce_product_loop()) {
+				woocommerce_product_loop_start();
 
-			/**
-			 * Hook: woocommerce_before_shop_loop.
-			 *
-			 * @hooked woocommerce_output_all_notices - 10
-			 * @hooked woocommerce_result_count - 20
-			 * @hooked woocommerce_catalog_ordering - 30
-			 */
-			do_action('woocommerce_before_shop_loop');
-
-			woocommerce_product_loop_start();
-
-			if (wc_get_loop_prop('total')) {
-				while (have_posts()) {
-					the_post();
-					/**
-					 * Hook: woocommerce_shop_loop.
-					 */
-					do_action('woocommerce_shop_loop');
-					wc_get_template_part('content', 'product');
+				if (wc_get_loop_prop('total')) {
+					while (have_posts()) {
+						the_post();
+						/**
+						 * Hook: woocommerce_shop_loop.
+						 */
+						do_action('woocommerce_shop_loop');
+						wc_get_template_part('content', 'product');
+					}
 				}
+
+				woocommerce_product_loop_end();
+				/**
+				 * Hook: woocommerce_after_shop_loop.
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action('woocommerce_after_shop_loop');
+			} else {
+				/**
+				 * Hook: woocommerce_no_products_found.
+				 *
+				 * @hooked wc_no_products_found - 10
+				 */
+				do_action('woocommerce_no_products_found');
 			}
 
-			woocommerce_product_loop_end();
 			/**
-			 * Hook: woocommerce_after_shop_loop.
+			 * Hook: woocommerce_after_main_content.
 			 *
-			 * @hooked woocommerce_pagination - 10
+			 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 			 */
-			do_action('woocommerce_after_shop_loop');
-		} else {
-			/**
-			 * Hook: woocommerce_no_products_found.
-			 *
-			 * @hooked wc_no_products_found - 10
-			 */
-			do_action('woocommerce_no_products_found');
-		}
-
-		/**
-		 * Hook: woocommerce_after_main_content.
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action('woocommerce_after_main_content');
-		?>
+			do_action('woocommerce_after_main_content');
+			?>
+		</div>
 	</div>
-
 </div>
 
 <?php
--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -36,9 +36,7 @@ get_header('shop'); ?>
 
 	<?php while (have_posts()) : ?>
 		<?php the_post(); ?>
-
 		<?php wc_get_template_part('content', 'single-product'); ?>
-
 	<?php endwhile; // end of the loop. 
 	?>
 
