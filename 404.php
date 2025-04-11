323d12e4ae6e2141df5872dc8d490b87b4741700
johnjovero98 <john.jovero@outlook.com>
2025-04-11 14:43:05 -0600

    Migrate privacy and 404 pages

--- 404.php
+++ 404.php
@@ -12,46 +12,16 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
+		<section class="error-404 not-found container mx-auto px-3 py-4">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'katppy-theme' ); ?></h1>
+				<h1 class="page-title text-center text-h1 mb-4"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'katppy-theme' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'katppy-theme' ); ?></p>
 
-					<?php
-					get_search_form();
+			<div class="flex justify-center">
+				<a class="button-404"href="<?php echo home_url(); ?>">Return to homepage</a>
+			</div>
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'katppy-theme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$katppy_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'katppy-theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$katppy_theme_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
