681b849219a70c438e6c1de483cf65bcbf8fdd51
johnjovero98 <john.jovero@outlook.com>
2025-04-15 02:21:42 -0600

    Header: refactor mobile navigation

--- header.php
+++ header.php
@@ -71,7 +71,7 @@
 				<div id="mobile-nav-overlay" class="overlay"></div><!-- mobile overlay -->
 				<nav id="mobile-site-navigation" class="mobile-navigation">
 					<!-- close button -->
-					<button id="close-button" class="lg:hidden" aria-controls="site-navigation" aria-expanded="false">
+					<button id="close-button" role="close navigation">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
 						</svg>
--- js/navigation.js
+++ js/navigation.js
@@ -1,22 +1,46 @@
 (function () {
 	console.log('navigation.js script is loaded purrr <3');
 
-	const chevronSVGTemplate = `
-	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="down-chevron">
-	  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-	</svg>
-  `;
+	const chevronSVGTemplate = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="down-chevron">
+									<path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+								</svg>`;
 
 	// add down chevron 
 	function appendChevronToElements(elements) {
 		document.querySelectorAll(elements).forEach((element) => {
 			element.insertAdjacentHTML('beforeend', chevronSVGTemplate);
 		});
-	}
+	};
 
 	// Add chevron to submenu items and currency widget
 	appendChevronToElements('.menu-item-has-children');
 	appendChevronToElements('.wmc-current-currency-arrow');
+
+	// Mobile Toggle Navigation
+	const menuToggleButton = document.getElementById('menu-toggle');
+	const menuCloseButton = document.getElementById('close-button');
+	const mobileNavOverlay = document.getElementById('mobile-nav-overlay');
+	const mobileMenu = document.getElementById('mobile-site-navigation');
+
+	function toggleMobileNavigation(button) {
+		button.addEventListener('click', () => {
+			mobileNavOverlay.classList.toggle('show-overlay');
+			const isExpanded = mobileMenu.classList.toggle('show-navigation');
+			menuToggleButton.setAttribute('aria-expanded', isExpanded);
+			mobileMenu.setAttribute('aria-hidden', !isExpanded);
+		});
+	}
+
+	// Initial ARIA state
+	menuToggleButton.setAttribute('aria-expanded', 'false');
+	menuToggleButton.setAttribute('aria-controls', 'mobile-site-navigation');
+	mobileMenu.setAttribute('aria-hidden', 'true');
+	mobileMenu.setAttribute('role', 'navigation'); 
+
+	// invoke toggle
+	toggleMobileNavigation(menuToggleButton);
+	toggleMobileNavigation(menuCloseButton);
+	toggleMobileNavigation(mobileNavOverlay);
 
 })();
 
